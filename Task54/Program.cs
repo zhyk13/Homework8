955e02e7f43cf2c33ca635131c6f75f4a35c9b78
zhyk <zhyk@list.ru>
2023-11-25 11:21:01 +0300

    Вторая задача

---
+++ Task54/Program.cs
@@ -0,0 +1,80 @@
+﻿// Реализуйте класс MatrixOperations, который содержит следующие статические методы:
+// PrintMatrix(int[,] matrix): Метод для вывода матрицы на экран. Он принимает на вход
+// двумерный массив целых чисел matrix и выводит его элементы в виде таблицы.
+// SortRowsDescending(int[,] matrix): Метод для сортировки строк матрицы по убыванию.
+// Он принимает на вход двумерный массив целых чисел matrix и сортирует каждую строку
+// матрицы так, чтобы элементы в каждой строке шли по убыванию.
+
+using System.ComponentModel.DataAnnotations;
+
+int InputIntNumber(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] printmatrix)
+{
+    for (int i = 0; i < printmatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < printmatrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{printmatrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] FillMatrixWhithRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] SortRowsDescending(int[,] matrix)
+{
+    int[] tmp = new int[matrix.GetLength(1)];
+    int min = matrix[0,0];
+    //int indeximin = 0;
+    int indexjmin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            min = matrix[i,j];
+            //int indeximin = i;
+            indexjmin = j;
+            for (int k = j; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i,k] < min)
+                {
+                    min = matrix[i,k];
+                    indexjmin = k;
+                }
+            }
+            matrix[i,indexjmin] = matrix[i,j];
+            matrix[i,j] = min;
+        }
+    }
+    return matrix;
+}
+
+
+int n = InputIntNumber("Введите кол-во строк массива: ");
+Console.WriteLine();
+int m = InputIntNumber("Введите кол-во столбцов массива: ");
+Console.WriteLine();
+int[,] matrix = new int[n,m];
+matrix = FillMatrixWhithRandom(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+matrix = SortRowsDescending(matrix);
+PrintMatrix(matrix);
+
---
+++ Task56/Program.cs
@@ -0,0 +1,83 @@
+﻿// Напишите программу для работы с матрицей целых чисел.
+// Реализуйте класс Answer, который содержит следующие статические методы:
+// SumOfRow(int[,] matrix, int row): Метод для вычисления суммы элементов в
+// заданной строке row матрицы matrix.
+// Метод принимает двумерный массив целых чисел matrix и номер строки row,
+// а возвращает целое число - сумму элементов в данной строке.
+// MinimumSumRow(int[,] matrix): Метод для определения строки с наименьшей
+// суммой элементов. Метод принимает двумерный массив целых чисел matrix и
+// возвращает массив из двух элементов: номер строки с наименьшей суммой
+// (нумерация начинается с 0) и саму сумму.
+
+using System.ComponentModel.DataAnnotations;
+
+int InputIntNumber(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrixWhithRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] printmatrix)
+{
+    for (int i = 0; i < printmatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < printmatrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{printmatrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumOfRow(int[,] matrix, int row)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        sum = sum + matrix[row, i];
+    }
+    return sum;
+}
+
+int[] MinimumSumRow(int[,] matrix)
+{
+    int[] numminrow = new int[2];
+    numminrow[0] = 0;
+    numminrow[1] = SumOfRow(matrix, 0);
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        if (numminrow[1] > SumOfRow(matrix, i))
+        {
+            numminrow[1] = SumOfRow(matrix, i);
+            numminrow[0] = i;
+        }
+    }
+    return numminrow;
+}
+
+int n = InputIntNumber("Введите кол-во строк массива: ");
+Console.WriteLine();
+int m = InputIntNumber("Введите кол-во столбцов массива: ");
+Console.WriteLine();
+int[,] matrix = new int[n,m];
+matrix = FillMatrixWhithRandom(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] rezult = new int[2];
+rezult = MinimumSumRow(matrix);
+Console.WriteLine($"Номер строки с минимальной суммой элементов - {rezult[0]}");
+Console.WriteLine($"Минимальная сумма элементов строки = {rezult[1]}");
+
